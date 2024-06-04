8a2a88586a4dacf6de0d05efefc0769d0bfd79c5
LEOOO!!! <120170440+ctrleo@users.noreply.github.com>
2024-06-04 08:22:29 +0000

    added login system

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-# init main.py
+import os, re, hashlib
+
+if os.path.exists(".passwords") == False:
+    print("Passwords not found! Creating now")
+    os.mkdir(".passwords")
+    user = input("Enter username: ")
+    specials = re.compile("[@_!#$%^&*()<>?/\|}{~:]")
+    while specials.search(user) != None:
+        print("Username cannot contain special characters!")
+        user = input("Enter username: ")
+    pw = input("Enter a new password: ")
+    utfpw = pw.encode("utf8")
+    hashpw = hashlib.sha256(utfpw)
+    password = hashpw.hexdigest()
+
+    profile = open(os.path.join(".passwords", user), "w")
+    profile.write(password)
+    profile.close()
+    print("User created successfully!")
+
+session = ""
+
+while session == "":
+    checkuser = input("Enter username: ")
+    if os.path.exists(os.path.join(".passwords", checkuser)) == False:
+        print("User not found, please try again!")
+    else:
+        checkpw = input("Enter password: ")
+        checkpw = checkpw.encode("utf8")
+        pwhash = hashlib.sha256(checkpw)
+        pwdigest = pwhash.hexdigest()
+        pwfile = open(os.path.join(".passwords", checkuser))
+        if pwfile.read() == pwdigest:
+            print("Authorized!")
+            session = checkuser
+        else:
+            print("Invalid Password! Try again...")
+
+print(f"User is {session}")
